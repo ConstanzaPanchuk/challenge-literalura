9820c93bf0993b12ea7c18ae1eb8431197d1ba73
ConstanzaPanchuk <constanza.panchuksteinmann@gmail.com>
2026-03-01 21:52:44 -0300

    Creación de clase ConsumoAPI y Clase Principal

---
+++ src/main/java/com/alura/challenge_literalura/principal/Principal.java
@@ -0,0 +1,9 @@
+package com.alura.challenge_literalura.principal;
+
+import com.alura.challenge_literalura.service.ConsumoAPI;
+
+public class Principal {
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String URL_BASE = "https://gutendex.com/books/";
+
+}
